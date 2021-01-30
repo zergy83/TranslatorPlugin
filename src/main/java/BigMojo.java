dbcf52241c0ca43a2959e2686a659103add7a7c7
Pierre M <zergconclave@hotmail.com>
2021-01-30 17:20:03 +0100

    implemented plugin

--- src/main/java/BigMojo.java
+++ src/main/java/BigMojo.java
@@ -1,16 +1,27 @@
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Translator generator.
  *
  */
-    @Mojo( name = "populate")
-    public class BigMojo extends AbstractMojo
-    {
-        public void execute() throws MojoExecutionException
+    @Mojo(name = "populate", defaultPhase = LifecyclePhase.COMPILE)
+    public class BigMojo extends AbstractMojo{
+    final static Logger LOGGER = LoggerFactory.getLogger(BigMojo.class);
+
+
+    public void execute() throws MojoExecutionException
         {
-            System.out.println("Hello mojo");
+           if(LOGGER.isDebugEnabled()){
+               LOGGER.debug(("Hello mojo"));
+           }
+
+           LOGGER.warn("Plug in is here");
+
+
         }
     }
